231dfa2aa94165d2309201d00c58074a2601b96b
Kristofer Bengtsson <kristofer@sekvensa.se>
2022-01-19 09:20:04 +0100

    updated the struct and started a new api

--- src/resources/ur.rs
+++ src/resources/ur.rs
@@ -3,109 +3,99 @@ use sp_domain::*;
 
 
 struct UrRobotResource{
-    trigger: SPPath,
-    goal_feature_name: SPPath,
-    tcp_name: SPPath,
-    command: SPPath,
-    acceleration: SPPath,
-    velocity: SPPath,
+    run: SPPath,  // Command. Trigger a robot motion
+    feedback: SPPath,  // Measured. feedback from robot
+    done: SPPath,   // Measured (changed by runner transitions). Motion completed
+    error: SPPath,  // Measured. Error from action
+    current_tcp_frame: SPPath,  // estimated. the tcp_frame that will be at the goal. Will change when a new motion is triggered.
+    current_tcp_frame_pose: SPPath  // estimated. The pose of the tcp_frame. Will be unknown, moving or the goal frame. 
+}
 
+impl UrRobotResource {
+    pub fn new(resource: &mut Resource, frame_domain: Vec<SPValue>) -> UrRobotResource {
+        let command= resource.add_variable(Variable::new(
+            "request/command",VariableType::Runner,SPValueType::String,vec!(),
+        ));
     
+        let acceleration= resource.add_variable(Variable::new(
+            "request/acceleration",VariableType::Runner,SPValueType::Float32,vec!(),
+        ));
+        let velocity= resource.add_variable(Variable::new(
+            "request/velocity",VariableType::Runner,SPValueType::Float32,vec!(),
+        ));
+        let goal_feature_name= resource.add_variable(Variable::new(
+            "request/goal_feature_name",VariableType::Runner,SPValueType::String, frame_domain.clone(),
+        ));
+        let tcp_name= resource.add_variable(Variable::new(
+            "request/tcp_name",VariableType::Runner,SPValueType::String,frame_domain.clone(),
+        ));
+    
+        let current_state= resource.add_variable(Variable::new(
+            "feedback/current_state", VariableType::Runner, SPValueType::String, vec!(),
+        ));
+    
+        let success= resource.add_variable(Variable::new_boolean(
+            "reply/success", VariableType::Runner
+        ));
+    
+        let trigger= Variable::new_boolean("trigger", VariableType::Runner);
+        let trigger= resource.add_variable(trigger);
+    
+        let _action_state = resource.setup_ros_action(
+            "URScriptControl",
+            "/ur_script_controller",
+            "ur_tools_msgs/action/URScriptControl",
+            p!(p: trigger),
+            // goal variables
+            &[
+                MessageVariable::new(&command, "command"),
+                MessageVariable::new(&acceleration, "acceleration"),
+                MessageVariable::new(&velocity, "velocity"),
+                MessageVariable::new(&goal_feature_name, "goal_feature_name"),
+                MessageVariable::new(&tcp_name, "tcp_name"),
+            ],
+            &[
+                // feedback.
+                MessageVariable::new(&current_state, "current_state"),
+            ],
+            &[
+                // result
+                MessageVariable::new(&success, "success"),
+            ],
+        );
+    
+        let run= resource.add_variable(Variable::new_boolean(
+            "command/run", VariableType::Command
+        ));
+        let done= resource.add_variable(Variable::new_boolean(
+            "measured/done", VariableType::Measured
+        ));
+        let error= resource.add_variable(Variable::new_boolean(
+            "measured/error", VariableType::Measured
+        ));
+        // Update the feedback domain
+        let feedback_domain: Vec<SPValue> = ["starting", "error", "done"].iter().map(|v| v.to_spvalue()).collect();
+        let feedback = resource.add_variable(Variable::new(
+            "measured/feedback",VariableType::Measured,SPValueType::String, feedback_domain,
+        ));
+        let current_tcp_frame = resource.add_variable(Variable::new(
+            "estimated/current_tcp_frame",VariableType::Estimated,SPValueType::String, frame_domain.clone(),
+        ));
+        let current_tcp_frame_pose = resource.add_variable(Variable::new(
+            "estimated/current_tcp_frame_pose",VariableType::Estimated,SPValueType::String, frame_domain.clone(),
+        ));
+
+        return UrRobotResource{
+            run,
+            feedback,
+            done,
+            error,
+            current_tcp_frame,
+            current_tcp_frame_pose
+        }
+    }
 }
 
-/// Make a UR robot resource that talks with the UR action
-/// The frame_domain defines the possible frames to move to defined in TF. The tcps are the 
-/// possible tcp frames of the robot that should end up at the frame. if it is not possible to 
-/// move a specific tcp to a frame, it must be restricted by invariants
-pub fn make_ur(resource: &mut Resource, frame_domain: Vec<SPValue>, tcps: Vec<SPValue>) { 
-    let command= resource.add_variable(Variable::new(
-        "request/command",VariableType::Command,SPValueType::String,vec!(),
-    ));
-
-    let acceleration= resource.add_variable(Variable::new(
-        "request/acceleration",VariableType::Runner,SPValueType::Float32,vec!(),
-    ));
-    let velocity= resource.add_variable(Variable::new(
-        "request/velocity",VariableType::Runner,SPValueType::Float32,vec!(),
-    ));
-    let goal_feature_name= resource.add_variable(Variable::new(
-        "request/goal_feature_name",VariableType::Command,SPValueType::String, frame_domain.clone(),
-    ));
-    let tcp_name= resource.add_variable(Variable::new(
-        "request/tcp_name",VariableType::Command,SPValueType::String,tcps,
-    ));
-
-    let current_state= resource.add_variable(Variable::new(
-        "feedback/current_state", VariableType::Runner, SPValueType::String, vec!(),
-    ));
-
-    let success= resource.add_variable(Variable::new_boolean(
-        "reply/success", VariableType::Measured
-    ));
-
-    let trigger= Variable::new_boolean("trigger", VariableType::Command);
-    let trigger= resource.add_variable(trigger);
-
-    let _action_state = resource.setup_ros_action(
-        "URScriptControl",
-        "/ur_script_controller",
-        "ur_tools_msgs/action/URScriptControl",
-        p!(p: trigger),
-        // goal variables
-        &[
-            MessageVariable::new(&command, "command"),
-            MessageVariable::new(&acceleration, "acceleration"),
-            MessageVariable::new(&velocity, "velocity"),
-            MessageVariable::new(&goal_feature_name, "goal_feature_name"),
-            MessageVariable::new(&tcp_name, "tcp_name"),
-        ],
-        &[
-            // feedback.
-            MessageVariable::new(&current_state, "current_state"),
-        ],
-        &[
-            // result
-            MessageVariable::new(&success, "success"),
-        ],
-    );
-
-    let actual_tool_frame = resource.add_variable(Variable::new(
-        "request/measured",VariableType::Measured,SPValueType::String, frame_domain,
-    ));
 
 
-    // create a fn instead on the struct to create new transitions in the scenario model
-
-
-    // for (p, v) in frames.iter() {
-    //     // restrict motions motions using invariants in the scenario model. 
-    //     // Copy this transition and use it 
-    //     resource.add_transition(
-    //         Transition::new(
-    //             &format!("to_{}_start", v.to_string()),
-    //             p!(!p: trigger),
-    //             vec![ a!( p: trigger)],
-    //             TransitionType::Controlled
-    //         )
-    //     );
-    //     resource.add_transition(
-    //         Transition::new(
-    //             &format!("to_{}_effect", v.to_string()),
-    //             p!([p: trigger]),
-    //             vec![ a!(!p: success)],
-    //             TransitionType::Effect
-    //         )
-    //     );
-    //     resource.add_transition(
-    //         Transition::new(
-    //             &format!("to_{}_fin", v.to_string()),
-    //             p!([p: trigger] && [p: success]),
-    //             vec![ a!(!p: trigger), a!(p: actual_tool_frame <- goal_feature_name)],
-    //             TransitionType::Auto
-    //         )
-    //     );
-
-    //}
-
-}
 
